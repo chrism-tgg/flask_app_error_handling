f80d64cc8c3ef0ae01eacf819103649b8316c6cb
chrism-tgg <chrism@guntergroup.com>
2022-06-04 13:42:24 -0700

    Simple try & except clause to handle errors at ../messages/3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
@@ -8,3 +8,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# URL variable idx to determine where messages will be displayed
+@app.route('/messages/<int:idx>')
+# define three messages.
+def message(idx):
+    messages = ['Message Zero', 'Message One', 'Message Two']
+    # Error handling with try...except clause
+    try:
+        return render_template('message.html', message=messages[idx])
+    except IndexError:
+        abort(404)
