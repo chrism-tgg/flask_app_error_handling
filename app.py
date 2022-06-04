af8386342b7a4c55444cb86d38eefd771707cb37
chrism-tgg <chrism@guntergroup.com>
2022-06-04 13:50:55 -0700

    Use error handler decorators for 404 & 500 errors.

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
-# There are two errors in this code: 
-# 1. did not import the render_template() function
-# 2. index.html template file does not exist.
+# Use special @app.errorhandler() decorator for 404 errors.
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+# This one relates to app.route 500 below
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -19,3 +26,8 @@ def message(idx):
         return render_template('message.html', message=messages[idx])
     except IndexError:
         abort(404)
+
+# ...
+@app.route('/500')
+def error500():
+    abort(500)
