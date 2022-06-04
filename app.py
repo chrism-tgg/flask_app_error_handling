c8bd0ba6a3dcea279198f54e8204363a316ad077
chrism-tgg <chrism@guntergroup.com>
2022-06-04 13:33:14 -0700

    Simple error handling with development FLASK_ENV

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# There are two errors in this code: 
+# 1. did not import the render_template() function
+# 2. index.html template file does not exist.
+@app.route('/')
+def index():
+    return render_template('index.html')
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Flask error handling
+
+using this tutorial to learn error handling in Flask.
+
+https://www.digitalocean.com/community/tutorials/how-to-handle-errors-in-a-flask-application
+
